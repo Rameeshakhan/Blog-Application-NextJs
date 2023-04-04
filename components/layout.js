bde06fc16c70e7a8bf41545841e891eb40839966
Rameesha Khan <rameeshakhan75@gmail.com>
2023-04-05 01:33:09 +0500

    Final Commit

--- components/layout.js
+++ components/layout.js
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/./image/image.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -43,7 +43,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <Image
                 priority
-                src="/images/profile.jpg"
+                src="/./image/image.jpg"
                 className={utilStyles.borderCircle}
                 height={108}
                 width={108}
@@ -59,6 +59,11 @@ export default function Layout({ children, home }) {
         )}
       </header>
       <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
     </div>
-  );
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/post';
+import Link from 'next/link';
+import Date from '../components/date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -11,20 +13,22 @@ export async function getStaticProps() {
     },
   };
 }
+
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
+      <p>This is rameesha Khan , A software Engineering Student. She is a JavaScript Developer and currently learning and growing in this domain.AI fascinates her a lot. Her future goal is to dive into Web 3.0.</p>
     <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
       <h2 className={utilStyles.headingLg}>Blog</h2>
       <ul className={utilStyles.list}>
         {allPostsData.map(({ id, date, title }) => (
-          <li className={utilStyles.listItem} key={id}>
-            {title}
-            <br />
-            {id}
-            <br />
-            {date}
-          </li>
+       <li className={utilStyles.listItem} key={id}>
+       <Link href={`/posts/${id}`}>{title}</Link>
+       <br />
+       <small className={utilStyles.lightText}>
+         <Date dateString={date} />
+       </small>
+     </li>
         ))}
       </ul>
     </section>
